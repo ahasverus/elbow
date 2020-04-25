f376279eb9afc79d38284fcbe07cd22db52fb076
Nicolas Casajus <nicolas.casajus@gmail.com>
2020-04-25 18:33:35 +0200

    :bulb: Edit Dataset Documentation

---
+++ R/profiles.R
@@ -0,0 +1,15 @@
+#' Profiles Dataset
+#'
+#' A data frame describing four concave profiles along an x-Axis.
+#'
+#' @format A data frame with 31 rows and 5 columns:
+#' \describe{
+#'   \item{x}{A sequence from 0 to 30 (x-Axis)}
+#'   \item{concave_down_pos_slo}{First concave profile (y-Axis): Concave down (positive slope)}
+#'   \item{concave_down_neg_slo}{Second concave profile (y-Axis): Concave down (negative slope)}
+#'   \item{concave_up_pos_slo}{Third concave profile (y-Axis): Concave up (positive slope)}
+#'   \item{concave_up_neg_slo}{Fourth concave profile (y-Axis): Concave up (negative slope)}
+#' }
+#'
+
+"profiles"
